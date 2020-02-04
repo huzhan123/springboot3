f77d2bdebe412c92d82dea0404fce0a0f411b93f
huzhan123 <345049517@qq.com>
2020-02-04 15:28:51 +0800

    测试组员A首次提交

---
+++ src/main/java/cn/com/huzhan/springboot/entity/test01.java
@@ -0,0 +1,13 @@
+package cn.com.huzhan.springboot.entity;
+
+public class test01 {
+   private  String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
